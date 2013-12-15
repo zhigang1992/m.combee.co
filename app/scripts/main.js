e405b3dddac42adacb80a56e2a61b98fd1b084db
Kyle Fang <zgfang@thoughtworks.com>
2013-12-15 22:14:30 +0800

    Added RequireJS basic, set up backbone.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+require.config({
+    paths: {
+        'jquery': '../bower_components/jquery/jquery',
+        'underscore': '../bower_components/underscore/underscore',
+        'backbone': '../bower_components/backbone/backbone'
+    }
+});
+
+require(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
+    console.log($);
+});
