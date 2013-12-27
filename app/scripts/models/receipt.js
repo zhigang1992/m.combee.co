409ed11e53d7e3ce167e731ec3c432fcdac381f4
Kyle Fang <zgfang@thoughtworks.com>
2013-12-28 01:48:16 +0800

    Add Receipt basic element and style

--- app/scripts/models/receipt.js
+++ app/scripts/models/receipt.js
@@ -1,6 +1,16 @@
 define(['backbone'], function(Backbone) {
 	var Recipt = Backbone.Model.extend({
-
+		title: function() {
+			return this.get('post').title;
+		},
+		avatar: function() {
+			return this.get('post').author.avatar;
+		},
+		mobileAvatar: function() {
+			var es = this.avatar().split('/');
+			es[es.length-1] = 'mobile_' + es[es.length-1];
+			return es.join('/');
+		}
 	});
 	return Recipt;
 });
